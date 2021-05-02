d02a5833a1d89d35fead1e14c15a22755d19086b
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-05-02 18:09:50 -0400

    Update heaps.py

--- HeapTraverse/heaps.py
+++ HeapTraverse/heaps.py
@@ -1,3 +1,5 @@
+
+
 import random 
 class MinHeap:
     def __init__(self):
@@ -111,20 +113,3 @@ class MinHeap:
 
                     
 
-
-
-
-lst = []
-
-heap = MinHeap()
-for i in range(10):
-    random_number = random.randint(1, 10)
-    lst.append(random_number)
-
-for i in lst:
-    heap.add(i)
-
-print(heap.heap_list)
-
-print(heap.traverse(5))
-    
